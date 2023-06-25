d75078e08ef3d3ec567e00c18ca3a303bbb0d3b7
frankind.eth <faguzzi3@gmail.com>
2023-06-25 16:25:01 -0300

    Configured Wagmi

--- src/config/wallet.ts
+++ src/config/wallet.ts
@@ -3,6 +3,7 @@ import { mainnet, goerli } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
 const apiKey = process.env.NEXT_PUBLIC_USE_TESTNET
   ? process.env.NEXT_PUBLIC_ALCHEMY_TESTNET_KEY
@@ -13,9 +14,15 @@ const { chains, publicClient } = configureChains(
   [alchemyProvider({ apiKey: apiKey ?? "" }), publicProvider()]
 );
 
+const { connectors } = getDefaultWallets({
+  appName: "Swaplace",
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ?? "",
+  chains,
+});
+
 const wagmiClientConfig = createConfig({
   autoConnect: true,
-  connectors: [new InjectedConnector({ chains })],
+  connectors,
   publicClient,
 });
 
