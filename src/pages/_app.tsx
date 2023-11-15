e78ffee976a9ef8689a5ebeb6039d1b658d146a2
heronlancellot <heronlancellot@id.uff.br>
2023-11-15 18:57:59 -0300

    Feat: Add Light Dark Modal Rainbow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import "tailwindcss/tailwind.css";
-
 import { chains, wagmiClientConfig } from "../config/wallet";
-
 import { AppProps } from "next/app";
 import { WagmiConfig } from "wagmi";
-import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
+import {
+  RainbowKitProvider,
+  lightTheme,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
 import { CookiesProvider } from "react-cookie";
 import { ThemeContextProvider } from "@/contexts/theme-config";
 
@@ -16,11 +18,17 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         <CookiesProvider>
           <WagmiConfig config={wagmiClientConfig}>
             <RainbowKitProvider
-              theme={lightTheme({
-                accentColor: "black",
-                borderRadius: "small",
-                overlayBlur: "small",
-              })}
+              theme={{
+                lightMode: lightTheme({
+                  accentColor: "black",
+                  borderRadius: "small",
+                  overlayBlur: "small",
+                }),
+                darkMode: darkTheme({
+                  borderRadius: "small",
+                  overlayBlur: "small",
+                }),
+              }}
               chains={chains}
             >
               <Component {...pageProps} />
